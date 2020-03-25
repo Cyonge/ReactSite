ea9cdc130633613fd07ebfb4186c1502631d3822
Christian Yonge <christianyonge93@gmail.com>
2020-03-25 14:06:37 -0500

    new portfolio using nextjs

---
+++ pages/contact.js
@@ -0,0 +1,205 @@
+
+import Head from 'next/head';
+import { Navbar, Nav } from 'react-bootstrap';
+
+
+const Contact = () => (
+  
+  <div className="container">
+    <Head>
+      <title>Contact</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
+
+    </Head>
+
+    <main>
+    <Navbar fixed="top" className collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/index">Christian Yonge</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Navbar>
+      
+
+      <div className="container">
+        <form>
+            <div className="form-group">
+                <label for="formGroupExampleInput">Name:</label>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Jane Doe"/>
+            </div>
+            <div className="form-group">
+                <label for="formGroupExampleInput2">Email:</label>
+                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="janedoe@deer.com"/>
+            </div>
+            <div className="form-group">
+                <label for="exampleFormControlTextarea1">Message for Christian:</label>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+                    placeholder="Message..."></textarea>
+            </div>
+            <button type="submit" className="btn btn-info">Submit</button>
+        </form>
+
+    </div>
+    </main>
+
+    <footer>
+      <a
+        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+      </a>
+    </footer>
+
+    <style jsx>{`
+      .container {
+        min-height: 100vh;
+        padding: 0 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid #eaeaea;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer img {
+        margin-left: 0.5rem;
+      }
+
+      footer a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      .title a {
+        color: #0070f3;
+        text-decoration: none;
+      }
+
+      .title a:hover,
+      .title a:focus,
+      .title a:active {
+        text-decoration: underline;
+      }
+
+      .title {
+        margin: 0;
+        line-height: 1.15;
+        font-size: 4rem;
+      }
+
+      .title,
+      .description {
+        text-align: center;
+      }
+
+      .description {
+        line-height: 1.5;
+        font-size: 1.5rem;
+      }
+
+      code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-size: 1.1rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+
+      .grid {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        max-width: 800px;
+        margin-top: 3rem;
+      }
+
+      .card {
+        margin: 1rem;
+        flex-basis: 45%;
+        padding: 1.5rem;
+        text-align: left;
+        color: inherit;
+        text-decoration: none;
+        border: 1px solid #eaeaea;
+        border-radius: 10px;
+        transition: color 0.15s ease, border-color 0.15s ease;
+      }
+
+      .card:hover,
+      .card:focus,
+      .card:active {
+        color: #0070f3;
+        border-color: #0070f3;
+      }
+
+      .card h3 {
+        margin: 0 0 1rem 0;
+        font-size: 1.5rem;
+      }
+
+      .card p {
+        margin: 0;
+        font-size: 1.25rem;
+        line-height: 1.5;
+      }
+
+      @media (max-width: 600px) {
+        .grid {
+          width: 100%;
+          flex-direction: column;
+        }
+      }
+    `}</style>
+
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+export default Contact
